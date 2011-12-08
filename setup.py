4284e623f7f7637e0451fe601f2966f879ddc233
Eric Mc Sween <eric.mcsween@auf.org>
2011-12-08 11:54:06 -0500

    Utiliser setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 # encoding: utf-8
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 name = 'auf.django.permissions'
+version = '0.1'
 
 setup(name=name,
-      version='0.1',
+      version=version,
       description="Extensions au système de permissions de Django",
       author='Éric Mc Sween',
       author_email='eric.mcsween@auf.org',
       url='http://pypi.auf.org/%s' % name,
       license='GPL',
-      packages=['auf.django.permissions'],
+      packages=find_packages(),
      )
